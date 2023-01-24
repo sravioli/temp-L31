e880a4582b78feea8a88cefc23a5a4be4849b0f0
Simone Fidanza <fsimone2002@gmail.com>
2023-01-24 21:39:03 +0100

    feat(mathjax): small rewrite of (almost) all LaTeX macros (WiP)
    rewrite most of the LaTeX macros to be more concise

--- docs/javascripts/mathjax.js
+++ docs/javascripts/mathjax.js
@@ -7,38 +7,72 @@ window.MathJax = {
         loader: {load: ['[tex]/textmacros']},
         tex: {packages: {'[+]': ['textmacros']}},
         macros: {
+            // numerical sets
             N: "{\\mathbb{N}}",
             Z: "{\\mathbb{Z}}",
             Q: "{\\mathbb{Q}}",
             R: "{\\mathbb{R}}",
             I: "{\\mathbb{I}}",
             C: "{\\mathbb{C}}",
+
+            // math operators
             mcd: "{\\mathop{\\rm mcd}\\nolimits}",
-            set: ["{\\left\\{\\, #1 \\,\\right\\}}", 1],
-            dom: "{\\mathop{\\rm dom\\,}\\nolimits}",
-            codom: "{\\mathop{\\rm codom\\,}\\nolimits}",
-            tc: "{\\hspace{0.5em}\\text{t.c.}\\hspace{0.5em}}",
-            ll: "{\\mathcal{L}}",
-            llo: "{\\mathcal{L}_O\\hspace{0.1em}}",
+            dom: "{\\mathop{\\rm dom}\\nolimits}",
+            codom: "{\\mathop{\\rm codom}\\nolimits}",
+
+            // so that
+            tc: "{\\hspace\{.4em}\\text{t.c.}\\hspace\{.4em}}",
+
+            // better set notation
+            set: ["{\\left\\{\\hspace\{.1em} #1 \\hspace\{.1em}\\right\\}}", 1],
+
+            // LdP macros
+            lng: "{\\mathcal{L}}",
+            llo: "{\\mathcal{L}_{\\hspace{-.08em}O} \\hspace\{.1em}}",
+
             mm: "{\\mathcal{M}}",
-            ml: "{\\mathcal{M}_{\\hspace{-0.10em}\\mathcal{L}}\\hspace{0.1em}}",
-            mlp: "{{\\mathcal{M}'}_{\\hspace{-0.45em}\\mathcal{L}^{'}}\\hspace{0.1em}}",
-            molo: "{{\\mathcal{M}_O}_{\\hspace{-0.1em}\\mathcal{L}_O}\\hspace{0.1em}}",
+            ml: "{{\\mm}_{\\hspace{-.15em} \\lng} \\hspace\{.1em}}",
+            mlprime: "{{\\mm'}_{\\hspace{-.55em} \\lng^{'}} \\hspace\{.1em}}",
+            molo: "{{\\mm_{\\hspace{-.1em}O}}_{\\hspace{-.1em}\\llo}\\hspace{.1em}}",
+
             prgm: "{\\mathcal{P}}",
             intr: "{\\mathcal{I}}",
             cmp: "{\\mathcal{C}}",
-            prgmcomp: "{\\mathcal{Pc}}",
-            sx: "{\\mathrm{\\,Sx\\,}}",
-            dx: "{\\mathrm{\\,Dx\\,}}",
+            prgmcmp: "{\\mathcal{Pc}}",
+
+            sx: "{\\mathop{\\rm Sx}\\nolimits}",
+            dx: "{\\mathop{\\rm Dx}\\nolimits}",
             caret: "{\\;\\hat{}\\;}",
-            bnf: ["{\\mathopen\\langle\\hspace{0.1em}\\text{#1}\\hspace{0.1em}\\mathclose\\rangle}", 1],
+            HALT: "{\\mathit{HALT}}",
+
+            // Backus-Naur form
+            bnf: [
+                `{\\mathopen\\langle\\hspace{.1em}
+                    \\text{#1}
+                 \\hspace{.1em}\\mathclose\\rangle}`,
+                1
+            ],
             Coloneqq: "{\\;\\mathop{::=}\\;}",
-            subimplies: ["{\\underset{\\substack{(#1)}}{\\implies}}", 1],
-            supimplies: ["{\\overset{\\substack{#1}}{\\implies}}", 1],
-            supsubimplies: ["{\\overset{\\substack{#1}}{\\underset{\\substack{#2}}{\\implies}} }", 2],
+
+            // Over/Under/Over-Under ==>
+            Oimplies: ["{\\overset{\\substack{#1}}{\\implies}}", 1],
+            Uimplies: ["{\\underset{\\substack{#1}}{\\implies}}", 1],
+            OUimplies: [
+                "{\\overset{\\substack{#1}}{\\underset{\\substack{#2}}{\\implies}}}",
+                2
+            ],
+
+
+
+            // subimplies: ["{\\underset{\\substack{(#1)}}{\\implies}}", 1],
+            // supimplies: ["{\\overset{\\substack{#1}}{\\implies}}", 1],
+            // supsubimplies: ["{\\overset{\\substack{#1}}{\\underset{\\substack{#2}}{\\implies}} }", 2],
             astimplies: "{\\overset{\\substack{\\ast}}{\\implies}}",
             deff: "{\\overset{\\substack{\\hspace{-0.1em}\\text{def}}}{\\iff}}",
+
+            // absolute value since it's not defined
             abs: ["{\\mathopen\\lvert #1\\mathclose\\rvert}", 1],
+
             XiV: "{X \\cap V}",
             XuV: "{X \\cup V}",
             XuVplus: "{{(X \\cup V)}^+}",
