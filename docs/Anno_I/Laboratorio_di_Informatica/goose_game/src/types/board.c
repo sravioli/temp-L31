7399759ce2ce969e4ef22782dcb57eb34d06eccf
Simone Fidanza <fsimone2002@gmail.com>
2023-04-14 13:13:43 +0200

    fix(rename): rename to `src/types`

--- docs/Anno_I/Laboratorio_di_Informatica/goose_game/src/types/board.c
+++ docs/Anno_I/Laboratorio_di_Informatica/goose_game/src/types/board.c
@@ -3,11 +3,9 @@
 int get_square(const Board *board, int position) {
   return board->squares[position];
 }
-
 void set_square(Board *board, int position, int value) {
   board->squares[position] = value;
 }
 
 int get_dim(const Board *board) { return board->dim; }
-
 void set_dim(Board *board, int dimension) { board->dim = dimension; }
