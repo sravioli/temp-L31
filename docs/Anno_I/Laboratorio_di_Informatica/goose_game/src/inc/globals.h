25af053e73e568dab5ce3054147fb67d6a8f600b
Simone Fidanza <fsimone2002@gmail.com>
2023-05-07 16:58:07 +0200

    fix(goose_game): document macros, move them to corresponding modules

--- docs/Anno_I/Laboratorio_di_Informatica/goose_game/src/inc/globals.h
+++ docs/Anno_I/Laboratorio_di_Informatica/goose_game/src/inc/globals.h
@@ -3,10 +3,7 @@
  *
  * @brief Global constants and definitions for the board game module.
  *
- * This file contains global constants and definitions used in all of the board
- * game modules. These constants define the minimum and maximum number of
- * squares and players allowed, values and positions of special squares on the
- * board, and other global settings for the game.
+ * This file contains some global default values used in the project.
  *
  * @warning This file should be included in any source file that needs to
  *          access these global constants and definitions.
@@ -28,57 +25,54 @@
 
 // GLOBAL CONSTANTS LIST
 
-// Minimum and maximum number of squares allowed
-#define MIN_NUM_SQUARES 50
-#define MAX_NUM_SQUARES 90
-
-// Minimum and maximum number of players allowed
-#define MIN_NUM_PLAYERS 2
-#define MAX_NUM_PLAYERS 4
-
-// Goose square spacing and value
-#define GOOSE_SPACING 9
-#define GOOSE_VALUE 0
-
-// Board squares positions
-#define BRIDGE_SQUARE 6
-#define INN_SQUARE 19
-#define WELL_SQUARE 31
-#define LABYRINTH_SQUARE 42
-#define PRISON_SQUARE 52
-#define SKELETON_SQUARE 58
-
-// Board squares values
-#define BRIDGE_VALUE -1
-#define INN_VALUE -2
-#define WELL_VALUE -3
-#define LABYRINTH_VALUE -4
-#define PRISON_VALUE -5
-#define SKELETON_VALUE -6
-
-// Default board properties
-#define DEFAULT_COLS 15
-#define DEFAULT_SQUARE_LEN 7
-#define DEFAULT_BORDERS                                                        \
-  { "╭", "╮", "╰", "╯", "┬", "┴", "─", "│" }
-
-// Player default properties
-#define INITIAL_POSITION 0
-#define INITIAL_SCORE 0
-#define MAX_NAME_LENGTH 20
-#define MAX_USERNAME_LENGTH 3
-
-// Dice properties
+/**
+ *  @def MIN_DICE_THROW
+ *  @brief The minimum allowed number that a dice throw can assume.
+ */
 #define MIN_DICE_THROW 1
+
+/**
+ *  @def MAX_DICE_THROW
+ *  @brief The maximum allowed number that a dice throw can assume.
+ */
 #define MAX_DICE_THROW 6
 
-// Other constants
+/**
+ *  @def LINE_END
+ *  @brief The character that defines a line termination.
+ */
 #define LINE_END "\n"
+
+/**
+ *  @def STR_END
+ *  @brief The character that defines the end of a string.
+ */
 #define STR_END '\0'
+
+/**
+ *  @def STR_EMPTY
+ *  @brief The character that defines an empty string.
+ */
 #define STR_EMPTY ""
+
+/**
+ *  @def SPACE_CHAR
+ *  @brief The default value that a space assumes.
+ */
 #define SPACE_CHAR ' '
 
+// boolean replacement (for visual clarity)
+
+/**
+ * @def FALSE
+ * @brief The default value for a boolean false. (used for visual clarity)
+ */
 #define FALSE 0
+
+/**
+ * @def TRUE
+ * @brief The default value for a boolean true. (used for visual clarity)
+ */
 #define TRUE (!FALSE)
 
 #endif  // GLOBALS_H
