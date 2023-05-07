d54321f20ddbc144651ab6ac0b5d97b8b16f25a3
Simone Fidanza <fsimone2002@gmail.com>
2023-05-07 16:41:41 +0200

    fix(goose_game): move `board` module to `types` folder

--- docs/Anno_I/Laboratorio_di_Informatica/goose_game/src/types/board.h
+++ docs/Anno_I/Laboratorio_di_Informatica/goose_game/src/types/board.h
@@ -1,9 +1,8 @@
 /**
- * @file board_type.h
+ * @file board.h
+ * @brief Header file for the @c Board struct and associated functions.
  *
- * @brief Header file for the Board struct and associated functions.
- *
- * This header file defines the Board struct, which represents a game board,
+ * This header file defines the @c Board struct, which represents a game board,
  * along with functions to manipulate the board, such as getting and setting
  * the value of a square, as well as getting and setting the dimension of the
  * board.
@@ -15,15 +14,148 @@
  *    Fidanza Simone
  *    Lecini Fabio
  *
- * @date 2023-04-22 15:49
- * @version 1.0
+ * @date 2023-05-05 16:02
+ * @version 2.0
  * @copyright GNU GPLv3
  */
 
 #ifndef BOARD_TYPE_H
 #define BOARD_TYPE_H
 
-#include "../inc/globals.h"
+#include "../common/inc/string.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+
+//-----------------------------------------------------------------------------
+//-----------------------------------------------------------------------------
+
+/**
+ * @def MIN_NUM_SQUARES
+ * @brief The minimum number of squares allowed on the board.
+ */
+#define MIN_NUM_SQUARES 50
+
+/**
+ * @def MAX_NUM_SQUARES
+ * @brief The maximum number of squares allowed on the board.
+ */
+#define MAX_NUM_SQUARES 90
+
+/**
+ * @def GOOSE_SPACING
+ * @brief The spacing between goose squares on the board.
+ */
+#define GOOSE_SPACING 9
+
+/**
+ * @def GOOSE_VALUE
+ * @brief The value of a goose square on the board.
+ */
+#define GOOSE_VALUE 0
+
+/**
+ * @def BRIDGE_SQUARE
+ * @brief The position of the bridge square on the board.
+ */
+#define BRIDGE_SQUARE 6
+
+/**
+ * @def INN_SQUARE
+ * @brief The position of the inn square on the board.
+ */
+#define INN_SQUARE 19
+
+/**
+ * @def WELL_SQUARE
+ * @brief The position of the well square on the board.
+ */
+#define WELL_SQUARE 31
+
+/**
+ * @def LABYRINTH_SQUARE
+ * @brief The position of the labyrinth square on the board.
+ */
+#define LABYRINTH_SQUARE 42
+
+/**
+ * @def PRISON_SQUARE
+ * @brief The position of the prison square on the board.
+ */
+#define PRISON_SQUARE 52
+
+/**
+ * @def SKELETON_SQUARE
+ * @brief The position of the skeleton square on the board.
+ */
+#define SKELETON_SQUARE 58
+
+/**
+ * @def BRIDGE_VALUE
+ * @brief The value of the bridge square on the board.
+ */
+#define BRIDGE_VALUE -1
+
+/**
+ * @def INN_VALUE
+ * @brief The value of the inn square on the board.
+ */
+#define INN_VALUE -2
+
+/**
+ * @def WELL_VALUE
+ * @brief The value of the well square on the board.
+ */
+#define WELL_VALUE -3
+
+/**
+ * @def LABYRINTH_VALUE
+ * @brief The value of the labyrinth square on the board.
+ */
+#define LABYRINTH_VALUE -4
+
+/**
+ * @def PRISON_VALUE
+ * @brief The value of the prison square on the board.
+ */
+#define PRISON_VALUE -5
+
+/**
+ * @def SKELETON_VALUE
+ * @brief The value of the skeleton square on the board.
+ */
+#define SKELETON_VALUE -6
+
+/**
+ * @def DEFAULT_COLS
+ * @brief The default number of columns in the board.
+ */
+#define DEFAULT_COLS 15
+
+/**
+ * @def DEFAULT_SQUARE_LEN
+ * @brief The default length of a square on the board.
+ */
+#define DEFAULT_SQUARE_LEN 7
+
+/**
+ * @def DEFAULT_BORDERS
+ * @brief The default borders to use when printing the board.
+ *
+ * This is an array of eight strings that represent the characters to use for
+ * the top-left, top-right, bottom-left, bottom-right, top, bottom, horizontal,
+ * and vertical borders of each square, respectively.
+ *
+ * @warning This array should be used as follows:
+ *          @code
+ *          const char *BORDERS[8] = DEFAULT_BORDERS;
+ *          @endcode
+ */
+#define DEFAULT_BORDERS                                                        \
+  { "╭", "╮", "╰", "╯", "┬", "┴", "─", "│" }
+
+//-----------------------------------------------------------------------------
+//-----------------------------------------------------------------------------
 
 /**
  * @brief A struct representing a board for a game.
@@ -50,7 +182,7 @@ typedef struct Board {
  * This function retrieves the value of a square on the board, given its
  * position.
  *
- * @param[in] board    Pointer to the Board struct.
+ * @param[in] board    Pointer to the @c Board struct.
  * @param[in] position Position of the square on the board.
  *
  * @return Value of the square at the given position.
@@ -63,7 +195,7 @@ int get_square(const Board *board, int position);
  * This function sets the value of a square on the board, given its position and
  * value.
  *
- * @param[in,out] board    Pointer to the Board struct.
+ * @param[in,out] board    Pointer to the @c Board struct.
  * @param[in]     position Position of the square on the board.
  * @param[in]     value    Value to set for the square.
  *
@@ -76,7 +208,7 @@ void set_square(Board *board, int position, int value);
  *
  * This function retrieves the dimension of the board.
  *
- * @param[in] board Pointer to the Board struct.
+ * @param[in] board Pointer to the @c Board struct.
  *
  * @return Dimension of the board.
  */
@@ -87,11 +219,52 @@ int get_dim(const Board *board);
  *
  * This function sets the dimension of the board.
  *
- * @param[in,out] board     Pointer to the Board struct.
+ * @param[in,out] board     Pointer to the @c Board struct.
  * @param[in]     dimension Dimension to set for the board.
  *
  * @return void.
  */
 void set_dim(Board *board, int dimension);
+
+//-----------------------------------------------------------------------------
+//-----------------------------------------------------------------------------
+
+/**
+ * @brief Build the game board as a string.
+ *
+ * This function takes a @c Board structure, along with other parameters, and
+ * creates a string representation of the game board. The resulting string
+ * includes the border characters specified in the @c borders array.
+ *
+ * @param[in] board      The @c Board structure to represent as a string
+ * @param[in] cols       The number of columns in the game board
+ * @param[in] square_len The length of one side of a square on the game board
+ * @param[in] borders    An array of eight characters to use as the border
+ *                       characters for the game board. The characters will be
+ *                       used in the following order:
+ *                       - nw_corner (ex. "┌")
+ *                       - ne_corner (ex. "┐")
+ *                       - sw_corner (ex. "└")
+ *                       - se_corner (ex. "┘")
+ *                       - join_down (ex. "┬")
+ *                       - join_up (ex. "┴")
+ *                       - dash (ex. "─")
+ *                       - vert (ex. "│")
+ * @return A string representation of the game board
+ */
+char *build_board(const Board board, const int cols, const int square_len,
+                  const char *borders[8]);
+
+/**
+ * @brief Prints the game board to stdout.
+ *
+ * This function prints the provided game board to stdout using the @c printf()
+ * function.
+ *
+ * @param board The game board to print.
+ *
+ * @return void.
+ */
+void print_board(char *board);
 
 #endif  // !BOARD_TYPE_H
