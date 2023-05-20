a95c8d4812870354a44135893e40533364f71364
Simone Fidanza <fsimone2002@gmail.com>
2023-05-20 17:11:05 +0200

    feat(goose_game): prototype help and leaderboard modules

---
+++ docs/Anno_I/Laboratorio_di_Informatica/goose_game/src/core/handle_help.c
@@ -0,0 +1,48 @@
+// Copyright (c) 2023 @authors. GNU GPLv3.
+// @authors
+//    Amorese Emanuele
+//    Blanco Lorenzo
+//    Cannito Antonio
+//    Fidanza Simone
+//    Lecini Fabio
+
+#include "../inc/handle_help.h"
+
+void print_help_menu() {
+  new_screen();
+  print_menu(HELP_MENU_FILE);
+}
+
+void display_file(const char filename[]) {
+  new_screen();
+  print_file(filename);
+  printf("%sExit this view by pressing b/ESC/ENTER/SPACEBAR%s%s", BOLD, RESET,
+         LINE_END);
+
+  int display = TRUE;
+  while (display) {
+    char key = _getch();
+    if (is_back_key(key)) {
+      display = FALSE;
+    }
+  }
+
+  print_help_menu();
+}
+
+void help_menu() {
+  print_help_menu();
+
+  int menu_loop = TRUE;
+  while (menu_loop) {
+    char key = _getch();
+
+    if (key == 'r') {
+      display_file(RULES_FILE);
+    } else if (key == 'm') {
+      display_file(MANUAL_FILE);
+    } else if (key == 'b') {
+      menu_loop = FALSE;
+    }
+  }
+}
--- docs/Anno_I/Laboratorio_di_Informatica/goose_game/src/core/handle_leaderboard.c
+++ docs/Anno_I/Laboratorio_di_Informatica/goose_game/src/core/handle_leaderboard.c
@@ -6,34 +6,96 @@
 //    Fidanza Simone
 //    Lecini Fabio
 
-#include "../inc/handle_leaderboard.h"
+// #include "../inc/handle_leaderboard.h"
+// #include "../inc/private/handle_leaderboard.h"
 
-Entries *allocate_entries() {
-  Entries *es = (Entries *)malloc(sizeof(Entries));  // NOLINT
-  if (!es) {
-    printf("error: failed to allocate memory for Entries struct.\n");
-    exit(EXIT_FAILURE);
-  }
-  return es;
+#include "../common/inc/logger.h"
+#include "../common/inc/string.h"
+#include "../common/inc/term.h"
+
+#include "../types/entries.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#include <string.h>
+#include <time.h>
+
+// ---------------------
+
+void append_entries(Entries *existing_entries, Entries *new_entries) {
+  // Copy the new entries to the end of the existing array
+  memcpy(get_entries(existing_entries) + get_num_entries(existing_entries),
+         get_entries(new_entries),
+         get_num_entries(new_entries) * sizeof(Entry));
+
+  // Update the number of entries
+  set_num_entries(existing_entries, get_num_entries(existing_entries) +
+                                        get_num_entries(new_entries));
 }
 
-Entries read_entries(const char filename[]) {
-  Entries *es = allocate_entries();
-
-  // read struct from given file
-  FILE *fp;
-  if (fopen_s(&fp, filename, "rb") != 0) {
-    printf("error: failed to open '%s'.\n", filename);
-    exit(EXIT_FAILURE);
-  }
-  int num_read_entries = fread(es, sizeof(Entries), 1, fp);
-  fclose(fp);
-
-  if (num_read_entries != 1) {
-    printf("error: failed to read Entreis struct from file\n");
-    free(es);
-    exit(EXIT_FAILURE);
-  }
-
-  return *es;
+void swap_entries(Entry *first, Entry *second) {
+  Entry temp = *first;
+  *first = *second;
+  *second = temp;
+  return;
 }
+
+void rm_duplicate_entries(Entries *es) {
+  int num_entries = get_num_entries(es);
+  int i = 0;
+  while (i < num_entries - 1) {
+    Entry *current = get_entry(es, i);
+    Entry *next = get_entry(es, i + 1);
+    if (strcmp(get_name(current), get_name(next)) == 0) {
+      int j = i + 1;
+      while (j < num_entries - 1) {
+        set_entry(es, get_entry(es, j + 1), j);
+        j = j + 1;
+      }
+      num_entries = num_entries - 1;
+      // Continue checking from this index to ensure no other duplicates were
+      // missed
+      i = i - 1;
+    }
+    i = i + 1;
+  }
+  return;
+}
+
+void sort_entries(Entries *es) {
+  int num_entries = get_num_entries(es);
+  int i = 0;
+  while (i < num_entries) {
+    int j = i + 1;
+    while (j < num_entries) {
+      Entry *ei = get_entry(es, i);
+      Entry *ej = get_entry(es, j);
+      int is_score_greater = get_final_score(ej) > get_final_score(ei);
+      int is_score_equal = get_final_score(ej) == get_final_score(ei);
+      int is_name_previous = strcmp(get_name(ej), get_name(ei)) < 0;
+      int should_swap_entries =
+          is_score_greater || (is_score_equal && is_name_previous);
+
+      if (should_swap_entries) {
+        swap_entries(ei, ej);
+      }
+      j = j + 1;
+    }
+    i = i + 1;
+  }
+  rm_duplicate_entries(es);
+
+  return;
+}
+
+// ----------------------------------------------------------------------------
+
+// int main(void) {
+//   logger.start("test.log");
+//   logger.log(__func__, "starting program");
+//
+//   logger.log(__func__, "stopping program");
+//   logger.stop();
+//   return 0;
+// }
