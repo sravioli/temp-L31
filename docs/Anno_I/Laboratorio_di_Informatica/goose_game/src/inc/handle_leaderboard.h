9011976926518f427f39bc903960620aa92b86d2
Simone Fidanza <fsimone2002@gmail.com>
2023-05-20 17:06:47 +0200

    fix(goose_game): create private includes, fix previous includes

--- docs/Anno_I/Laboratorio_di_Informatica/goose_game/src/inc/handle_leaderboard.h
+++ docs/Anno_I/Laboratorio_di_Informatica/goose_game/src/inc/handle_leaderboard.h
@@ -3,9 +3,30 @@
 #ifndef LEADERBOARD_MODULE_H
 #define LEADERBOARD_MODULE_H
 
+#include "../common/inc/logger.h"
+#include "../common/inc/string.h"
+#include "../common/inc/term.h"
+
 #include "../types/entries.h"
 
 #include <stdio.h>
 #include <stdlib.h>
+
+#include <string.h>
+#include <time.h>
+
+/**
+ * @brief Appends entries to an existing array.
+ *
+ * This function appends new entries to an existing @c Entries array.
+ *
+ * @param[in,out] existing_entries A pointer to the existing @c Entries struct
+ *                                 to append to.
+ * @param[in]     new_entries      A pointer to the @c Entries struct containing
+ *                                 the new entries to append.
+ *
+ * @return void.
+ */
+void append_entries(Entries *existing_entries, Entries *new_entries);
 
 #endif  // !LEADERBOARD_MODULE_H
--- docs/Anno_I/Laboratorio_di_Informatica/goose_game/src/inc/private/handle_game.h
+++ docs/Anno_I/Laboratorio_di_Informatica/goose_game/src/inc/private/handle_game.h
@@ -1,6 +1,48 @@
+// Copyright (c) 2023 @authors. GNU GPLv3.
+
+/**
+ * @file private/handle_game.h
+ * @brief ...
+ *
+ * ...
+ *
+ * @authors
+ *    Amorese Emanuele
+ *    Blanco Lorenzo
+ *    Cannito Antonio
+ *    Fidanza Simone
+ *    Lecini Fabio
+ *
+ * @date ...
+ * @version 1.0
+ * @copyright GNU GPLv3
+ */
+
 #ifndef GAME_MODULE_PRIVATE_H
 #define GAME_MODULE_PRIVATE_H
 
+#include "../handle_game.h"
+
+void conform_username(char *username);
 int is_username_valid(const char username[]);
+char *ask_username(Players *pls, const int current_player);
+
+void selection_sort_players(Players *players, int *dice_values);
+
+void interactive_roll(Players *pls, int *dice_values);
+void interactive_reroll(Players *pls, int *dice_values);
+void print_players_list(Players *pls);
+
+int check_square(const Board *board, int position);
+void insert_goose_squares(Board *board);
+void insert_special_squares(Board *board);
+
+char *sq_to_str(const int square);
+
+char *build_border(const char *borders[4], const int square_len, const int cols,
+                   const int rows, const int row, const Board board);
+
+char *build_squares(const char *vert, const int square_len, const int cols,
+                    const int rows, const int row, const Board board);
 
 #endif  // !GAME_MODULE_PRIVATE_H
