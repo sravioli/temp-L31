4cdb7d11d2a27c0a0b94676669f0461ce8cd6139
Simone Fidanza <fsimone2002@gmail.com>
2023-05-23 15:20:44 +0200

    feat(goose_game): create files to store error messages and input messages

---
+++ docs/Anno_I/Laboratorio_di_Informatica/goose_game/src/inc/inputs.h
@@ -0,0 +1,20 @@
+#ifndef INPUT_FORMATS_H
+#define INPUT_FORMATS_H
+
+#define ASK_BOUND_VALUE_FMT "Insert the desired number of %s [%d, %d]: "
+
+#define ASK_USERNAME_FMT "Player %i, enter your username: "
+#define PRINT_VALID_USERNAME_FMT "your username will be: %s\n"
+
+#define USERNAME_SCREEN_TITLE "\nUSERNAME SELECTION\n"
+
+#define PRESS_TO_ROLL_FMT "%s press any key to roll dice"
+#define ROLL_RESULT_FMT "%s rolled a %d\n"
+
+#define PRESS_TO_REROLL_FMT "%s press any key to re-roll"
+#define REROLL_RESULT_FMT "%s rerolled and got a %d\n"
+
+#define DICE_TIE_MSG "Tie detected! Rerolling for tied players:\n"
+#define FINAL_ORDER_MSG "Based on the dice rolls, the turn order is:\n"
+
+#endif  // !INPUT_FORMATS_H
