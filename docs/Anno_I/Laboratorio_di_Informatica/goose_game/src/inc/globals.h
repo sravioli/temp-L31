6830db203b2a30f20271a64ffd54830b36915650
Simone Fidanza <fsimone2002@gmail.com>
2023-06-01 17:30:37 +0200

    fix(goose_game): change path to res since it has been moved

--- docs/Anno_I/Laboratorio_di_Informatica/goose_game/src/inc/globals.h
+++ docs/Anno_I/Laboratorio_di_Informatica/goose_game/src/inc/globals.h
@@ -44,14 +44,14 @@
 #define DECIMAL_BASE 10
 
 #define MAIN_MENU "./res/text/menus/main.txt"
-#define PAUSE_MENU "./res/text/menus/pause.txt"
-#define HELP_MENU "./res/text/menus/help.txt"
-#define SAVED_MENU "./res/text/menus/saved.txt"
+#define PAUSE_MENU "../../res/text/menus/pause.txt"
+#define HELP_MENU "../../res/text/menus/help.txt"
+#define SAVED_MENU "../../res/text/menus/saved.txt"
 
-#define RULES_FILE "./res/text/rules.txt"
-#define MANUAL_FILE "./res/text/manual.txt"
+#define RULES_FILE "../../res/text/rules.txt"
+#define MANUAL_FILE "../../res/text/manual.txt"
 
-#define SAVED_GAMES_FILE "./res/saves.bin"
-#define LEADERBOARD_FILE "./res/leaderboard.bin"
+#define SAVED_GAMES_FILE "../../res/saves.bin"
+#define LEADERBOARD_FILE "../../res/leaderboard.bin"
 
 #endif  // GLOBALS_H
