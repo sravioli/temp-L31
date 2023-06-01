b56c382afdc7eaf4b197ebc56fbe31a108c2bc16
Simone Fidanza <fsimone2002@gmail.com>
2023-06-01 17:29:56 +0200

    fix(goose_game): use library fns instead of new ones

--- docs/Anno_I/Laboratorio_di_Informatica/goose_game/src/core/handle_help.c
+++ docs/Anno_I/Laboratorio_di_Informatica/goose_game/src/core/handle_help.c
@@ -15,30 +15,8 @@
 
 #include "../inc/handle_help.h"
 
-void print_help_menu() {
-  new_screen();
-  print_menu(HELP_MENU);
-}
-
-void display_file(const char filename[]) {
-  new_screen();
-  print_file(filename);
-  printf("%sExit this view by pressing b/ESC/ENTER/SPACEBAR%s%s", BOLD, RESET,
-         LINE_END);
-
-  int display = TRUE;
-  while (display) {
-    char key = _getch();
-    if (is_back_key(key)) {
-      display = FALSE;
-    }
-  }
-
-  print_help_menu();
-}
-
 void help_menu() {
-  print_help_menu();
+  print_menu(HELP_MENU);
 
   int menu_loop = TRUE;
   while (menu_loop) {
@@ -46,8 +24,10 @@ void help_menu() {
 
     if (key == 'r') {
       display_file(RULES_FILE);
+      print_menu(HELP_MENU);
     } else if (key == 'm') {
       display_file(MANUAL_FILE);
+      print_menu(HELP_MENU);
     } else if (key == 'b') {
       menu_loop = FALSE;
     }
