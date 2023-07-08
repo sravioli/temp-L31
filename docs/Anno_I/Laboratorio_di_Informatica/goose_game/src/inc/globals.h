8fa54654b9facf6211719e92109f058b2fb2eb16
Simone Fidanza <fsimone2002@proton.me>
2023-07-08 12:41:37 +0200

    feat(goose_game): add documentation for global constants

--- docs/Anno_I/Laboratorio_di_Informatica/goose_game/src/inc/globals.h
+++ docs/Anno_I/Laboratorio_di_Informatica/goose_game/src/inc/globals.h
@@ -15,8 +15,8 @@
  *    Fidanza Simone
  *    Lecini Fabio
  *
- * @date 2023-04-17 13:13
- * @version 1.0
+ * @date 2023-07-08 12:40
+ * @version 1.5
  * @copyright GNU GPLv3
  */
 
@@ -39,19 +39,54 @@
  */
 #define TRUE (!FALSE)
 
+/**
+ * @brief Return value indicating that an index was not found.
+ */
 #define INDEX_NOT_FOUND -1
 
+/**
+ * @brief Base for decimal numbers.
+ */
 #define DECIMAL_BASE 10
 
+/**
+ * @brief Path to the main menu text file.
+ */
 #define MAIN_MENU "../res/text/menus/main.txt"
+
+/**
+ * @brief Path to the pause menu text file.
+ */
 #define PAUSE_MENU "../res/text/menus/pause.txt"
+
+/**
+ * @brief Path to the help menu text file.
+ */
 #define HELP_MENU "../res/text/menus/help.txt"
+
+/**
+ * @brief Path to the saved menu text file.
+ */
 #define SAVED_MENU "../res/text/menus/saved.txt"
 
+/**
+ * @brief Path to the rules text file.
+ */
 #define RULES_FILE "../res/text/rules.txt"
+
+/**
+ * @brief Path to the manual text file.
+ */
 #define MANUAL_FILE "../res/text/manual.txt"
 
+/**
+ * @brief Path to the saved games binary file.
+ */
 #define SAVED_GAMES_FILE "../res/data/saves.bin"
+
+/**
+ * @brief Path to the leaderboard binary file.
+ */
 #define LEADERBOARD_FILE "../res/data/leaderboard.bin"
 
 #endif  // GLOBALS_H
