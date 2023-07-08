636b69775108f4a7622b56475fc925c78411f9a8
Simone Fidanza <fsimone2002@proton.me>
2023-07-08 12:01:02 +0200

    feat(goose_game): make `saved_games()` public

--- docs/Anno_I/Laboratorio_di_Informatica/goose_game/src/inc/handle_saving.h
+++ docs/Anno_I/Laboratorio_di_Informatica/goose_game/src/inc/handle_saving.h
@@ -2,8 +2,14 @@
 #define SAVING_MODULE_H
 
 #include "../common/inc/types/board.h"
+#include "../common/inc/types/gamestate.h"
+#include "../common/inc/types/gamestates.h"
 #include "../common/inc/types/players.h"
 
+// -------------------------------------------------------------------------- //
+// -------------------------------------------------------------------------- //
+
 void save_game(Players *pls, Board board);
+void saved_games();
 
 #endif  // !SAVING_MODULE_H
